7553c36ef6530167451c1e4af5d15511978cdfac
Sandbox <sandbox@factory>
2026-03-19 09:22:52 +0000

    feat: TASK-4766 - Create version handler with build variable injection

--- internal/handler/version.go
+++ internal/handler/version.go
@@ -1,7 +1,18 @@
 package handler
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
 
-// VersionHandler serves build version information. Not yet implemented.
+	"github.com/robotiqdev/project-13/internal/version"
+)
+
+// VersionHandler serves build version information.
 func VersionHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		writeError(w, http.StatusMethodNotAllowed, "method not allowed")
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(version.Info())
 }
